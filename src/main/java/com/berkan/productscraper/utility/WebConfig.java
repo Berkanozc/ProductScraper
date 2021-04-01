5b4064d4dbb312979f11712a766472755e81ae77
ozcakib <berkan.ozcakir@hva.nl>
2021-04-01 21:05:06 +0200

    style: Add Cors mapping

---
+++ src/main/java/com/berkan/productscraper/utility/WebConfig.java
@@ -0,0 +1,34 @@
+package com.berkan.productscraper.utility;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${jwt.issuer:private company}")
+    public String issuer;
+
+    @Value("${jwt.pass-phrase:secret }")
+    public String passPhrase;
+
+    @Value("${jwt.duration-of-validity:2400}")
+    public int tokenDurationOfValidity;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
+                .exposedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
+                .allowedMethods("GET", "PUT", "POST", "DELETE",
+                        "PATCH", "OPTIONS", "HEAD");
+        ;
+    }
+
+}
